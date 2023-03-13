bc47f8dd59fbfe05fb1d024d55aee7e7de9b4048
MarcoCilona <marco.cilona@gmail.com>
2023-03-13 23:26:32 +0100

    Added nav menu on mobile with hamburger

---
+++ script.js
@@ -0,0 +1,35 @@
+const hamburgerMenu = document.querySelector(".hamburger");
+const navigationItems = document.querySelector(".navigation-items");
+const closeButton = document.querySelector(".close-btn");
+
+const toggleHamburgerMenu = () => {
+  if (hamburgerMenu.getAttribute("aria-expanded") == "true") {
+    closeMenu();
+    return;
+  }
+
+  openMenu();
+};
+
+/**
+ * Arrow function
+ */
+const openMenu = () => {
+  // Hamburger menu
+  hamburgerMenu.setAttribute("aria-expanded", true);
+
+  // Navigation items
+  navigationItems.setAttribute("aria-hidden", false);
+};
+
+const closeMenu = () => {
+  // Hamburger menu
+  hamburgerMenu.setAttribute("aria-expanded", false);
+
+  // Navigation items
+  navigationItems.setAttribute("aria-hidden", true);
+};
+
+hamburgerMenu.addEventListener("click", toggleHamburgerMenu, false);
+
+closeButton.addEventListener("click", closeMenu, false);
